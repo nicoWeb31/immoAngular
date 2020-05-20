4b33fe84ed9bc9f2bc84d8d57db01a7ab0950171
Riot Nicolas <nico.riot@free.fr>
2020-05-20 11:56:45 +0200

    crud complet sur les bien

--- src/app/admin/admin-properties/admin-properties.component.ts
+++ src/app/admin/admin-properties/admin-properties.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PropertiesService } from '../../services/properties.service';
+import { Subscription } from 'rxjs';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-admin-properties',
@@ -9,15 +12,35 @@ import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class AdminPropertiesComponent implements OnInit {
 
 
+
+
   propertiesForm: FormGroup;
+  propSubscription: Subscription;
+  properties: any[] = [];
+
+  indexToRemove;
+  indexToUpdate;
+  editMode = false;
+
+
 
   constructor(
     //service form builder
-    private formBuilder : FormBuilder
+    private formBuilder : FormBuilder,
+    private popServ :PropertiesService
   ) { }
 
   ngOnInit(): void {
+
     this.initPropertiesForm();
+    this.popServ.propertiesSubject.subscribe(
+      (data) =>{
+        //data dans mon tableau porertie
+        this.properties = data;
+      }
+    );
+    //emission des donner
+    this.popServ.emitProperties();
   }
 
 
@@ -37,7 +60,77 @@ export class AdminPropertiesComponent implements OnInit {
     //console.log(form.value)
     // const t = form.value['title']
     // console.log(t);
-    console.log(this.propertiesForm.value)
+    //console.log(this.propertiesForm.value)
+
+    const newProperty = this.propertiesForm.value;
+    //log du new tableau
+    //console.log(this.properties);
+    //si edit mode on fait un update sinon un crerate
+
+    if(this.editMode){
+      this.popServ.udapdateProperties(newProperty,this.indexToUpdate)
+
+    }else{
+      this.popServ.createPropertie(newProperty);
+    }
+    $('#propertiesFormModal').modal('hide');
+
+
+  }
+
+
+  //reset form
+  resteForm(){
+    this.propertiesForm.reset();
+    this.editMode=false;
+  }
+
+  //delete prpperties
+  onDelteProp(index){
+    //console.log(this.properties[index])
+    //avec message d'alert:
+    // if(confirm("etes-vous sur?")) this.popServ.deleteProperties(index);
+
+    //avec la modale:
+    $('#exampleModalLong').modal('show');
+    //je passe l'id pour la supression dans onConfirmDeletePrpop
+    this.indexToRemove = index;
+
+  }
+
+  onConfirmDeletePrpop(){
+    this.popServ.deleteProperties(this.indexToRemove);
+    $('#exampleModalLong').modal('hide');
+  }
+
+
+  // ===========================================================================
+  // onEdit
+  // ===========================================================================
+  onEditPrprop(property){
+
+
+    //on passe en mode edit
+    this.editMode = true;
+
+    //console.log(property);
+    $('#propertiesFormModal').modal('show');
+    this.propertiesForm.get('title').setValue(property.title);
+    this.propertiesForm.get('category').setValue(property.category);
+    this.propertiesForm.get('surface').setValue(property.surface);
+    this.propertiesForm.get('room').setValue(property.room);
+    this.propertiesForm.get('descr').setValue(property.descr);
+    this.propertiesForm.get('price').setValue(property.price);
+
+    const i = this.properties.findIndex(
+      (propEl)=>{
+        if(propEl === property) return true
+      }
+    )
+    this.indexToUpdate = i;
+
+
+
 
   }
 
--- src/app/services/properties.service.ts
+++ src/app/services/properties.service.ts
@@ -9,10 +9,10 @@ export class PropertiesService {
 
 
   properties = [
-    {title:'Ma super maison',category:'Maison',sold:true},
-    {title:'Villa sur la mer',category:'Maison',sold:false},
-    {title:'Appart maraca',category:'appart',sold:true},
-    {title:'petit apprt minable',category:'Appart',sold:false}
+    {title:'Ma super maison',category:'Maison',sold:true,price:3400000,room:4,surface:344},
+    {title:'Villa sur la mer',category:'Maison',sold:false,price:24000,room:4,surface:344},
+    {title:'Appart maraca',category:'Appartement',sold:true,price:134000,surface:344},
+    {title:'petit apprt minable',category:'Appartement',sold:false,price:31000,surface:344}
 
   ]
 
@@ -27,6 +27,40 @@ export class PropertiesService {
   }
 
 
+  // ===========================================================================
+  // get properties
+  // ===========================================================================
   getProperties(){}
+
+
+  // ===========================================================================
+  // create properties
+  // ===========================================================================
+
+  createPropertie(property){
+    this.properties.push(property);
+  }
+
+  // ===========================================================================
+  // delete properties
+  // ===========================================================================
+  deleteProperties(index){
+    // splice je supprime a partir de l'index un nombre d'element ici 1
+    this.properties.splice(index,1);
+    //on met a jour le tableau
+    this.emitProperties();
+  }
+
+  // ===========================================================================
+  // udapdateProperties update
+  // ===========================================================================
+
+  udapdateProperties(prop,i){
+    //la propiete a l'index i prend la nouvelle valeur
+      this.properties[i] = prop;
+      //on emmet
+      this.emitProperties();
+  }
+
 
 }
